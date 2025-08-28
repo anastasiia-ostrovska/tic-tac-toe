f049b4882d02bcd2d7cc91f93e3adb9f9948adbf
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-08-28 18:57:48 +0300

    refactor: add symbol icons to game-cell component

--- src/features/game/ui/game-cell.tsx
+++ src/features/game/ui/game-cell.tsx
@@ -1,6 +1,6 @@
 import { memo } from 'react';
 import { Button } from '@/shared/ui/button';
-import type { GameCell as Cell, GameCellClickHandler } from '@/entities/game';
+import { type GameCell as Cell, type GameCellClickHandler, GameSymbolIcon } from '@/entities/game';
 
 interface GameCellProps {
   id: number;
@@ -10,8 +10,8 @@ interface GameCellProps {
 
 const GameCell = ({ id, value, onClick }: GameCellProps) => {
   return (
-    <Button className="p-0 h-10 w-10 rounded-none" onClick={() => onClick(id)}>
-      {value}
+    <Button variant="outline" className="p-0 h-10 w-10 rounded-none" onClick={() => onClick(id)}>
+      {value ? <GameSymbolIcon symbol={value} className="w-5 h-5" /> : value}
     </Button>
   );
 };
