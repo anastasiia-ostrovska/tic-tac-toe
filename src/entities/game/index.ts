3e30867b5082b9029614ea215a1409104b8fa9b3
Anastasiia Ostrovska <95317301+anastasiia-ostrovska@users.noreply.github.com>
2025-08-29 00:32:47 +0300

    feat: add a general use-game hook slip into handler and timer hooks; add initial-state config; (#6)

--- src/entities/game/index.ts
+++ src/entities/game/index.ts
@@ -1,5 +1,6 @@
 export * from './model/types';
 export * from './consts/game';
 
+export { generateInitialCells } from './lib/generate-initial-cells';
 export { calculateWinner } from './model/calculate-winner';
 export { default as GameSymbolIcon } from './ui/game-symbol-icon';
