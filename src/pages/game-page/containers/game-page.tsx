dcaacadb0384a2240c76dc358be513595f2a3cc9
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-08-29 08:09:35 +0300

    feat: add game-over-dialog

--- src/pages/game-page/containers/game-page.tsx
+++ src/pages/game-page/containers/game-page.tsx
@@ -1,15 +1,15 @@
 import { Button } from '@/shared/ui/button';
 import { Timer } from '@/shared/ui/timer';
-import { GameBoard } from '@/features/game';
 import { GameSymbolIcon } from '@/entities/game';
 import { PlayerCard } from '@/features/player';
 import { SelectBoardSize } from '@/features/game';
+import { GameBoard, GameOverDialog } from '@/features/game';
 import GamePageLayout from '../ui/game-page-layout';
 import { useGame } from '../model/use-game';
 
 const GamePage = () => {
   const {
-    gameState: { cells, totalGames, currentGameTime, currentPlayer },
+    gameState: { cells, totalGames, currentGameTime, currentPlayer, winner },
     playersState,
     boardSize,
     handleCellClick,
@@ -17,20 +17,23 @@ const GamePage = () => {
   } = useGame();
 
   return (
-    <GamePageLayout
-      playerCards={
-        <>
-          <PlayerCard player={playersState.x} />
-          <PlayerCard player={playersState.o} />
-        </>
-      }
-      nextMoveIcon={<GameSymbolIcon symbol={currentPlayer} className="w-4 h-4" />}
-      gameTimer={<Timer totalSeconds={currentGameTime} />}
-      gameBoard={<GameBoard cells={cells} onClick={handleCellClick} size={boardSize} />}
-      newGameButton={<Button onClick={handleNewGameClick}>New Game</Button>}
-      selectSize={<SelectBoardSize />}
-      playedGamesCount={totalGames}
-    />
+    <>
+      <GamePageLayout
+        playerCards={
+          <>
+            <PlayerCard player={playersState.x} />
+            <PlayerCard player={playersState.o} />
+          </>
+        }
+        nextMoveIcon={<GameSymbolIcon symbol={currentPlayer} className="w-4 h-4" />}
+        gameTimer={<Timer totalSeconds={currentGameTime} />}
+        gameBoard={<GameBoard cells={cells} onClick={handleCellClick} size={boardSize} />}
+        newGameButton={<Button onClick={handleNewGameClick}>New Game</Button>}
+        selectSize={<SelectBoardSize />}
+        playedGamesCount={totalGames}
+      />
+      <GameOverDialog winner={winner} />
+    </>
   );
 };
 
